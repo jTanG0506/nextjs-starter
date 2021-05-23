1128b1e35dd1fdfdd5808555f6666eb5c3dc63b6
Jonathan Tang <hello@jonathantang.dev>
2021-05-23 17:15:07 +0100

    Initial commit from Create Next App

---
+++ pages/_app.tsx
@@ -0,0 +1,8 @@
+import type { AppProps } from 'next/app'
+import '../styles/globals.css'
+
+const App = ({ Component, pageProps }: AppProps): JSX.Element => {
+  return <Component {...pageProps} />
+}
+
+export default App
